31e58a7224ac491bcb4162a479c76feb6eff062d
ashafa90 <ashafabuhari@gmail.com>
2018-11-30 23:38:07 +0530

    adding POJOs. Close #2

---
+++ src/main/java/com/axasolution/mypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.axasolution.mypetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/axasolution/mypetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.axasolution.mypetclinic.model;
+
+public class Vet extends Person {
+
+}
